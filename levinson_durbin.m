e5c6d6ed8686c8d6274036006b52dab288a61fe4
Matt Staib <mstaib@stanford.edu>
2015-03-15 15:11:40 -0700

    Finds R upper-triangular with inv(toeplitz(z)) = R*R' as in Alkire and Vandengerghe

---
+++ levinson_durbin.m
@@ -0,0 +1,18 @@
+function R = levinson_durbin(z)
+
+z = z(:);
+n = length(z);
+E = diag(ones(n-1,1), -1);
+
+R = zeros(n);
+
+R(1,1) = 1/sqrt(z(1));
+
+for k=1:(n-1)
+    rk_tilde = [flipud(R(1:k,k)); zeros(n-k,1)];
+    alpha_k = -R(k,k) * z' * E * R(:,k);
+
+    R(:,k+1) = 1/sqrt(1-alpha_k^2) * (E*R(:,k) + alpha_k*rk_tilde);
+end
+
+end
