3ecb2e0392187471701f4da97029c70ab03bc998
Matt Staib <mstaib@stanford.edu>
2015-03-18 14:59:36 -0700

    More code cleanup

--- phase1_central_path.m
+++ phase1_central_path.m
@@ -24,20 +24,5 @@ while 1
     tau = nu / (s' * x);
 
     % v = sigma_tau(z_k)
-    u = x;
-    while 1
-        R = levinson_durbin(u);
-        grad_u = barrier_grad(u, R);
-        hess_u = barrier_hess(u, R);
-        [d_u, y_u] = delta(c, A, b, grad_u, hess_u, tau);
-
-        lambda = sqrt(d_u' * hess_u * d_u);
-        if lambda > beta
-            break
-        end
-
-        u = u + d_u / (1 + lambda);
-    end
-
-    v = u;
+    v = sigma_tau(x, tau, c, A, b, beta);
 end
---
+++ sigma_tau.m
@@ -0,0 +1,16 @@
+function u = sigma_tau(x, tau, c, A, b, beta)
+
+u = x;
+while 1
+    R = levinson_durbin(u);
+    grad_u = barrier_grad(u, R);
+    hess_u = barrier_hess(u, R);
+    [d_u, y_u] = delta(c, A, b, grad_u, hess_u, tau);
+
+    lambda = sqrt(d_u' * hess_u * d_u);
+    if lambda > beta
+        break
+    end
+
+    u = u + d_u / (1 + lambda);
+end
